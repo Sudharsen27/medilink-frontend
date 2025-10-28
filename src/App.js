2d0570e743f3e4ef3fed79b987a36c52d4bf8f41
Sudharsen27 <sundarlingam272000@gmail.com>
2025-10-28 15:49:42 +0530

    check deploy

--- src/App.js
+++ src/App.js
@@ -313,7 +313,6 @@ import React, { useState, useEffect } from 'react';
 import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
-import DarkModeToggle from './components/DarkModeToggle';
 
 // Components
 import Dashboard from './components/Dashboard';
